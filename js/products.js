a17a4a631932bd76a48d3aff3fefe935deaa0784
julianterraordiozola <julian.terraordiozola@gmail.com>
2021-08-12 10:42:42 -0300

    cambio login

--- js/products.js
+++ js/products.js
@@ -4,3 +4,13 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+
+//PASO 1: OBTENER LA INFORMACIÓN Product URL  : Inits.js
+// Paso 2: UTILIZAMOS LA FUNCION FETCH: En init.js
+// paso 3: (orientacion) Ver en category la funcion. 
+//paso 4: Poner URL de productos y ver coomo funciona en alguna consola   = await.
+//paso 5 podemos usar FETCH o GETJASON DATA  en init.js
+//Paso 6: Luego de que tenemos la informacion hacemos un procedimiento para insertarlo en el HTML
+//paso 7: traemos los datos y lo guardamos en una variable, y con comillas invertidas (En contenido) y lo metemos con inner html
+//PASO 8: Hacer procedimiento para insertar en HTML el primer producto
+// PASO 9: hacer procedimiento para insertar todos los productos en el HTML

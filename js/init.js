85c5d1419cdba0b3a565955cdd2bc7dfa5deaea9
julianterraordiozola <julian.terraordiozola@gmail.com>
2021-11-11 11:38:12 -0300

    login google 6

--- js/init.js
+++ js/init.js
@@ -69,6 +69,25 @@ function mostrar_usuario() {
 }
 
 
+// SCRIPT GOOGLE
+function onSignIn(googleUser) {
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+sessionStorage.setItem("Email", profile.getEmail());
+location.replace('index.html');
+
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
+
+  // The ID token you need to pass to your backend:
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+}
+
 //Lista de eventos a ejecutar//
 document.addEventListener("DOMContentLoaded", function(e) {
     mostrar_usuario();
